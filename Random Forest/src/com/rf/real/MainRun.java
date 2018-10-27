11828fafd59623bbed7678c754b601f5ed330cbb
Farzana Aktar <sinceramuna@gmail.com>
2018-10-28 00:29:39 +0600

    Add files via upload

---
+++ Random Forest/src/com/rf/real/MainRun.java
@@ -0,0 +1,28 @@
+package com.rf.real;
+
+import java.util.ArrayList;
+
+public class MainRun {
+	@SuppressWarnings("static-access")
+	public static void main(String args[]){
+
+		String traindata="Data.txt";
+		String testdata="Test.txt";
+		int numTrees=9;
+		
+		DescribeTrees DT = new DescribeTrees(traindata);
+		ArrayList<int[]> Input=DT.CreateInput(traindata);int categ=0;
+		
+		DescribeTrees DTT = new DescribeTrees(traindata);
+		ArrayList<int[]> Test=DTT.CreateInput(testdata);
+		
+		for(int k=0;k<Input.size();k++){if(Input.get(k)[Input.get(k).length-1]<categ)continue;else{categ=Input.get(k)[Input.get(k).length-1];}}
+		
+		RandomForest RaF =new RandomForest(numTrees, Input, Test);
+		RaF.C=categ;
+		RaF.M=Input.get(0).length-1;
+		RaF.Ms=(int)Math.round(Math.log(RaF.M)/Math.log(2)+1);
+		RaF.Start();
+
+	}
+}
---
+++ Random Forest/src/com/rf/real/categ/MainRun.java
@@ -0,0 +1,49 @@
+package com.rf.real.categ;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainRun {
+	public static void main(String[] args){
+		System.out.println("Random-Forest with Categorical support");
+		System.out.println("Now Running");
+		/*
+		 * data has to be separated by either ',' or ' ' only...
+		 */
+		int categ=0;
+		String traindata,testdata;
+		if(categ>0){
+			traindata="KDDTrainSmall.txt";
+			testdata="KDDTestSmall.txt";
+		}else if(categ<0){
+			traindata="Data.txt";
+			testdata="Test.txt";
+		}else{
+			traindata="KDDTrainSmall.txt";
+			testdata="KDDTestSmall.txt";
+		}
+		
+		DescribeTreesCateg DT = new DescribeTreesCateg(traindata);
+		ArrayList<ArrayList<String>> Train = DT.CreateInputCateg(traindata);
+		ArrayList<ArrayList<String>> Test = DT.CreateInputCateg(testdata);
+		/*
+		 * For class-labels 
+		 */
+		HashMap<String, Integer> Classes = new HashMap<String, Integer>();
+		for(ArrayList<String> dp : Train){
+			String clas = dp.get(dp.size()-1);
+			if(Classes.containsKey(clas))
+				Classes.put(clas, Classes.get(clas)+1);
+			else
+				Classes.put(clas, 1);				
+		}
+		
+		int numTrees=10;
+		int M=Train.get(0).size()-1;
+		int Ms = (int)Math.round(Math.log(M)/Math.log(2)+1);
+		int C = Classes.size();
+		RandomForestCateg RFC = new RandomForestCateg(numTrees, M, Ms, C, Train, Test);
+		RFC.Start();
+		
+	}
+}
