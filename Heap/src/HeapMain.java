dd14edfbc872c53514971199e5ef6d0ed2ebcfb6
Farzana Aktar <sinceramuna@gmail.com>
2018-10-27 22:48:35 +0600

    Add files via upload

---
+++ Heap/src/HeapMain.java
@@ -0,0 +1,13 @@
+
+public class HeapMain {
+	public static void main(String[] args) {
+		Heap heap= new Heap();
+		int A[]= {0,3,5,8,7,1,13,11,15,6};
+		int last= A.length-1;
+		A=heap.BuildHeap(A,last);
+		
+		heap.HeapSort(A, last);
+		System.out.println("Heap Sort: ");
+		heap.PrintHeap(A);
+	}
+}
